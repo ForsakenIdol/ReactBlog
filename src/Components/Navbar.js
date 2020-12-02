d8c1ab84400a089b38326ee715d03246d6142479
Lachlan D Whang <lachlandwhang@gmail.com>
2020-12-02 14:30:35 +0800

    Space and tab fix

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,20 +1,20 @@
 function Navbar(logged_in) {
     return (
         <nav className="navbar navbar-expand-md navbar-light navigation container">
-            <button type="button" className="navbar-toggler" data-toggle="collapse" data-target="#navigationlinks">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navigationlinks">
-        <ul class="navbar-nav mr-auto" id="navlinks-left">
-            <li class="nav-item"><a href="#blog">Home</a></li>
-          <li class="nav-item"><a href="#main-content">About</a></li>
-          <li class="nav-item"><a href="#contact">Contact</a></li>
-        </ul>
-        <ul class="navbar-nav ml-auto" id="navlinks-right">
-          <li class="nav-item"><a href="#login">Login</a></li>
-          <li class="nav-item"><a href="#register" class="register">Register</a></li>
-        </ul>
-      </div>
+          <button type="button" className="navbar-toggler" data-toggle="collapse" data-target="#navigationlinks">
+              <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navigationlinks">
+            <ul class="navbar-nav mr-auto" id="navlinks-left">
+                <li class="nav-item"><a href="#blog">Home</a></li>
+              <li class="nav-item"><a href="#main-content">About</a></li>
+              <li class="nav-item"><a href="#contact">Contact</a></li>
+            </ul>
+            <ul class="navbar-nav ml-auto" id="navlinks-right">
+              <li class="nav-item"><a href="#login">Login</a></li>
+              <li class="nav-item"><a href="#register" class="register">Register</a></li>
+            </ul>
+          </div>
             
         </nav>
     );
