60a23e832f78928fe1fe46d8c20f39f06f22fdfb
Lachlan D Whang <lachlandwhang@gmail.com>
2020-12-02 12:30:10 +0800

    Updated navbar and title card styling

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,10 +1,22 @@
 function Navbar(logged_in) {
     return (
-        <ul className="navigation">
-            <li><a href="#">Home</a></li>
-            <li><a href="#">About</a></li>
-            <li><a href="#">Contact</a></li>
+        <nav className="navbar navbar-expand-md navbar-light navigation container">
+            <button type="button" className="navbar-toggler" data-toggle="collapse" data-target="#navigationlinks">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navigationlinks">
+        <ul class="navbar-nav mr-auto" id="navlinks-left">
+            <li class="nav-item"><a href="#blog">Home</a></li>
+          <li class="nav-item"><a href="#main-content">About</a></li>
+          <li class="nav-item"><a href="#contact">Contact</a></li>
         </ul>
+        <ul class="navbar-nav ml-auto" id="navlinks-right">
+          <li class="nav-item"><a href="#login">Login</a></li>
+          <li class="nav-item"><a href="#register" class="register">Register</a></li>
+        </ul>
+      </div>
+            
+        </nav>
     );
 }
 
