c0e4d9b9ccaa7596b2c782f04db0c25c20aa001a
ForsakenIdol <lachlandwhang@gmail.com>
2020-12-03 19:35:37 +0800

    Converted PostCard into a functional component

--- src/Components/PostCard.js
+++ src/Components/PostCard.js
@@ -1,52 +1,38 @@
 // The post card contains information about a blog post that has not been featured.
 // All posts will be displayed in this form, including those which are also featured in a Feature Card.
 
-import React from 'react';
-
-class PostCard extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            title: this.props.title,
-            description: this.props.description,
-            image: this.props.image,
-            postWidth: this.props.postWidth
-        }
+function PostCard(props) {
+    let className = "post-card col-";
+    switch (props.postWidth) {
+        case 1:
+            className += '1';
+            break;
+        case 2:
+            className += '2';
+            break;
+        case 3:
+            className += '3';
+            break;
+        case 4:
+            className += '4';
+            break;
+        case 6:
+            className += '6';
+            break;
+        case 12:
+            className += '12';
+            break;
+        default:
+            className += '6';
+            break;
     }
-
-    render() {
-        let className = "post-card col-";
-        switch (this.state.postWidth) {
-            case 1:
-                className += '1';
-                break;
-            case 2:
-                className += '2';
-                break;
-            case 3:
-                className += '3';
-                break;
-            case 4:
-                className += '4';
-                break;
-            case 6:
-                className += '6';
-                break;
-            case 12:
-                className += '12';
-                break;
-            default:
-                className += '6';
-                break;
-        }
-        return (
-            <div className={className}>
-                <img src={this.state.image} alt="placeholder" class="post-image" />
-                <h3 className="post-title">{this.state.title}</h3>
-                <p>{this.state.description}</p>
-            </div>
-        );
-    }
+    return (
+        <div className={className}>
+            <img src={props.image} alt="placeholder" className="post-image" />
+            <h3 className="post-title">{props.title}</h3>
+            <p>{props.description}</p>
+        </div>
+    );
 }
 
 export default PostCard;
