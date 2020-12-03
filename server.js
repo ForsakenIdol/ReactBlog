398a80d797e480e4e903d332f876388e2b491b46
ForsakenIdol <lachlandwhang@gmail.com>
2020-12-03 20:36:01 +0800

    Installed mysql, dotenv, and express packages to set up server for mysql requests

---
+++ server.js
@@ -0,0 +1,33 @@
+/* Our server will be responsible for providing a wrapper for the MySQL package. We'll wrap it in Express and serve some routes for our application. */
+
+const mysql = require('mysql');
+const dotenv = require('dotenv');
+const path = require('path');
+const express = require('express');
+
+const app = express();
+
+dotenv.config({ path: path.resolve(__dirname + "/private/config.env") }); // Load environmental variables
+
+// Create the connection to our MySQL database
+const db = mysql.createConnection({
+  host: process.env.MYSQLDIGITALOCEANHOST,
+  user: process.env.MYSQLDIGITALOCEANUSER,
+  password: process.env.MYSQLDIGITALOCEANPWD,
+  database: process.env.MYSQLDB,
+  port: process.env.MYSQLDIGITALOCEANPORT
+});
+
+/* Setting up the port for our application */
+const port = 8080;
+const server = app.listen(port, () => {
+  console.log(`My website is now live on port ${port}.`);
+  db.connect(function(err) {
+    if (err) console.log(err);
+    else console.log("Connected to MYSQL!");
+  });
+});
+
+/* Request paths */
+
+app.get('/', (req, res) => {res.send("Hello World!");});
--- src/Pages/BlogHome.js
+++ src/Pages/BlogHome.js
@@ -8,9 +8,11 @@ import PostBox from '../Sections/PostBox';
 // The constructor can later initialize this component with state regarding the blog post information.
 // For now, we'll hardcode everything.
 class BlogHome extends React.Component {
+
     // Setting state in componentDidMount will trigger a re-render, just like it did for my weather app.
     componentDidMount() {
-        
+        console.log("Blog mounted!");
+        // Here we'll fetch the data from our server, which will probably be on something like "localhost:8080", and set state using that data (this.setState()).
     }
 
     render() {
