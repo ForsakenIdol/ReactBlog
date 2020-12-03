c457a1e860e1a5d5ed721c2203f1ea431b9c7754
Lachlan D Whang <lachlandwhang@gmail.com>
2020-12-03 08:06:21 +0800

    Updated test material for featured box

--- src/Sections/FeaturedBox.js
+++ src/Sections/FeaturedBox.js
@@ -6,14 +6,14 @@ import FeatureCard from '../Components/FeatureCard';
 class FeaturedBox extends React.Component {
 
     render() {
-        let desc1 = "This is a test description. It is meant to take up a fair amount of space so that the styling and width of this paragraph element can be tested, and so that the size can be adjusted if needed.";
+        let desc1 = "This is a test description. It is meant to take up a fair amount of space so that the styling and width of this paragraph element can be tested, and so that the size can be adjusted if needed. HD 16:9 and widescreen image resolutions work the best as backgrounds for these images.";
         let desc2 = "This is another paragraph element. It is significantly shorter than the previous one.";
         return (
         <div className="featured-box row">
             <div className="col-2" />
             <div className="col-8">
                 <h2 className="section-header" >Featured Posts</h2>
-                <FeatureCard title="A sample card" description={desc1} image={process.env.PUBLIC_URL + '/squareimg1.png'}/>
+                <FeatureCard title="A sample card" description={desc1} image={process.env.PUBLIC_URL + '/wideimg1.jpg'}/>
                 <FeatureCard title="Another sample" description={desc2} image={process.env.PUBLIC_URL + '/squareimg2.png'}/>
             </div>
         </div>
