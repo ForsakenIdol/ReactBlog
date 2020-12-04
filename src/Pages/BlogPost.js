42a7ac9a791d21a7b225a78f395798932079b386
Lachlan D Whang <lachlandwhang@gmail.com>
2020-12-04 15:14:31 +0800

    Lifted sample comments up to BlogPost from CommentBox

--- src/Pages/BlogPost.js
+++ src/Pages/BlogPost.js
@@ -14,15 +14,29 @@ class BlogPost extends React.Component {
         let datetime = "20/11/2020 14:00:00";
         let content = "This is test content.\nLet's see how this renders!"
 
+        let comments = [
+            {
+                author: "ForsakenIdol",
+                datetime: "22/11/2020 13:31:55",
+                content: "A fantastic piece of work!",
+                admin: true
+            },
+            {
+                author: "Legionator",
+                datetime: "22/11/2020 13:45:27",
+                content: "You'd think that at 21, people would have a pretty decent idea of where they want to go in industry. Most people in college are in their senior year, myself a rising senior going into honours, and even though you might not have your sights set on the ideal job for you just yet, you probably have a decent idea of what that's going to be, or what kind of work you want to be doing once you get into it. Myself, I'd link to think I've got a decent idea of where I want to go - probably something cybersecurity related or freelancing web development on the side - but I'm still not sure if software is even the right path for me. I feel like I've been doing what I'm meant to be doing, and not necessarily what I want to be doing. And yet, here I am - 2 years into this degree, because there's nothing else out there that I really want to be doing."
+            }
+        ]
+
         return (
             <div className="blog-post">
                 <Navbar />
                 <BlogBox image={image} title={title} author={author} datetime={datetime} content={content}/>
                 <CommentForm />
-                <CommentBox />
+                <CommentBox comments={comments}/>
             </div>
         );
-    }
+    }   
 
 }
 
--- src/Sections/CommentBox.js
+++ src/Sections/CommentBox.js
@@ -21,23 +21,10 @@ class CommentBox extends React.Component {
 
     render() {
         let header = this.props.comments && this.props.comments.length > 0 ? `Comments (${this.props.comments.length})` : "Comments";
-        let comments = [
-            {
-                author: "ForsakenIdol",
-                datetime: "22/11/2020 13:31:55",
-                content: "A fantastic piece of work!",
-                admin: true
-            },
-            {
-                author: "Legionator",
-                datetime: "22/11/2020 13:45:27",
-                content: "You'd think that at 21, people would have a pretty decent idea of where they want to go in industry. Most people in college are in their senior year, myself a rising senior going into honours, and even though you might not have your sights set on the ideal job for you just yet, you probably have a decent idea of what that's going to be, or what kind of work you want to be doing once you get into it. Myself, I'd link to think I've got a decent idea of where I want to go - probably something cybersecurity related or freelancing web development on the side - but I'm still not sure if software is even the right path for me. I feel like I've been doing what I'm meant to be doing, and not necessarily what I want to be doing. And yet, here I am - 2 years into this degree, because there's nothing else out there that I really want to be doing."
-            }
-        ]
         return (
             <div class="container comment-box">
                 <h3 class="comment-section-title">{header}</h3>
-                {this.renderComments(comments)}
+                {this.renderComments(this.props.comments)}
             </div>
         );
     }
