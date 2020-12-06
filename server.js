c806e7a409cb4b87517db7959f8998e334ab7138
Lachlan D Whang <lachlandwhang@gmail.com>
2020-12-07 07:50:27 +0800

    Added sample post contents to the server API

--- server.js
+++ server.js
@@ -54,6 +54,30 @@ app.get('/api/blog/posts', (req, res) => {
   });
 });
 
+app.get('/api/blog/samplepost', (req, res) => {
+  let post = {
+    title: "A Test Post",
+    author: "ForsakenIdol",
+    datetime: "20/11/2020 14:00:00",
+    image: process.env.PUBLIC_URL + '/wideimg1.jpg',
+    content: "This is test content.\nLet's see how this renders!"
+  }
+  let comments = [
+      {
+          author: "ForsakenIdol",
+          datetime: "22/11/2020 13:31:55",
+          content: "A fantastic piece of work!",
+          admin: true
+      },
+      {
+          author: "Legionator",
+          datetime: "22/11/2020 13:45:27",
+          content: "You'd think that at 21, people would have a pretty decent idea of where they want to go in industry. Most people in college are in their senior year, myself a rising senior going into honours, and even though you might not have your sights set on the ideal job for you just yet, you probably have a decent idea of what that's going to be, or what kind of work you want to be doing once you get into it. Myself, I'd link to think I've got a decent idea of where I want to go - probably something cybersecurity related or freelancing web development on the side - but I'm still not sure if software is even the right path for me. I feel like I've been doing what I'm meant to be doing, and not necessarily what I want to be doing. And yet, here I am - 2 years into this degree, because there's nothing else out there that I really want to be doing."
+      }
+  ]
+  res.send((post, comments));
+});
+
 app.get('/api/blog/posts/:id', (req, res) => {
   db.query("SELECT * FROM post WHERE id = ?", [req.params.id], (err, result, fields) => {
     if (err) {
