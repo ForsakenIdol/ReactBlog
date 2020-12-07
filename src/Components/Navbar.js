91b608e8f9a7b6f20c409594ed0892005c3e66c3
Lachlan D Whang <lachlandwhang@gmail.com>
2020-12-07 15:12:32 +0800

    Incorporated react-router with paths for the homepage, about (temporary function), and the generic post page.

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,37 +1,47 @@
 import React from 'react';
+import { BrowserRouter as Router, Link } from 'react-router-dom';
+
+/*
+ * ~~~~~~~~~~ Great React Router Pages ~~~~~~~~~~
+ * - Code example of React Router use: https://reactrouter.com/web/example/basic
+ *   Note that we can have certain routes defined in our switch with the corresponding route function, but which are not
+ *   captured in the route object.
+ * - Using variables in a route path: https://soshace.com/this-is-how-i-created-a-simple-app-using-react-routing/
+ */
 
 class Navbar extends React.Component {
   render_auth(logged_in) {
     if (!logged_in) return (
       <ul className="navbar-nav ml-auto" id="navlinks-right">
-        <li className="nav-item"><a href="#login">Login</a></li>
-        <li className="nav-item"><a href="#register" className="register">Register</a></li>
+        <li className="nav-item"><Link to='/login'>Login</Link></li>
+        <li className="nav-item"><Link to='/register' className="register">Register</Link></li>
       </ul>
     );
     else return (
       <ul className="navbar-nav ml-auto" id="navlinks-right">
-        <li className="nav-item"><a href="#profile">Profile</a></li>
-        <li className="nav-item"><a href="#logout">Logout</a></li>
+        <li className="nav-item"><Link to='/profile'>Profile</Link></li>
+        <li className="nav-item"><Link to='/logout'>Logout</Link></li>
       </ul>
     );
   }
 
   render() {
     return (
-      <nav className="navbar navbar-expand-md navbar-light navigation container">
-        <button type="button" className="navbar-toggler" data-toggle="collapse" data-target="#navigationlinks">
-            <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navigationlinks">
-          <ul className="navbar-nav mr-auto" id="navlinks-left">
-              <li className="nav-item"><a href="#blog">Home</a></li>
-            <li className="nav-item"><a href="#main-content">About</a></li>
-            <li className="nav-item"><a href="#contact">Contact</a></li>
-          </ul>
-          {this.render_auth(this.props.logged_in)}
-        </div>
-          
-      </nav>
+      <Router>
+        <nav className="navbar navbar-expand-md navbar-light navigation container">
+          <button type="button" className="navbar-toggler" data-toggle="collapse" data-target="#navigationlinks">
+              <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navigationlinks">
+            <ul className="navbar-nav mr-auto" id="navlinks-left">
+              <li className="nav-item"><Link to='/'>Home</Link></li>
+              <li className="nav-item"><Link to='/about'>About</Link></li>
+              <li className="nav-item"><Link to='/contact'>Contact</Link></li>
+            </ul>
+            {this.render_auth(this.props.logged_in)}
+          </div>
+        </nav>
+      </Router>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -4,11 +4,28 @@ import 'bootstrap/dist/js/bootstrap';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import BlogHome from './Pages/BlogHome';
 import BlogPost from './Pages/BlogPost';
 
 ReactDOM.render(
-    <BlogPost />,
+    <Router>
+        <Switch>
+          {
+          // This will need some logic which identifies variables in the path URL and uses them to request the blog data
+          // which matches that particular variable (ID for posts)
+          }
+          <Route path='/post' component={BlogPost} />
+          <Route path='/about'>
+            <About />
+          </Route>
+          <Route path='/' component={BlogHome} />
+        </Switch>
+    </Router>,
     document.getElementById('root')
 );
+
+function About() {
+    return <h2>The about page.</h2>;
+  }
