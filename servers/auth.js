61e36e500628b49275e907aff83c29f738114a72
ForsakenIdol <lachlandwhang@gmail.com>
2020-12-09 20:51:40 +0800

    Added secrets to config.env file and tied the actual backend into the login route

--- servers/auth.js
+++ servers/auth.js
@@ -38,10 +38,11 @@ const server = auth.listen(port, () => {
   });
 });
 
-// Temporary secrets - these will be put into environmental variables during production.
+// Put these secrets into the config.env file manually
 let ACCESS_TOKEN_SECRET = "cfacd1ca6ef54c01adefe522d56ed668bfcd8db73f7d43426fa0787ec8284c807ed16ca1b296f39e8def9011bc2825dd1d41e7fc63ef363d1ad4346418a721e1";
 let REFRESH_TOKEN_SECRET = "e3f71fdf637bff5f647a56b01c09d78e0367768c96e1ed8d6e39e73047a375abcc92ad47587f77f170d2e96db9641f0d2f225d4aa60eee540defddcd3ac0bd88";
 
+// This data is not to be used unless the database is not available.
 let tempUsers = [
   {
     id: 1,
@@ -49,23 +50,25 @@ let tempUsers = [
     email: "jd1234@example.com",
     password_plain: "veryweakpassword",
     password_hash: "$2b$10$mBKis30z6XqpLGJq0F3LAuzxZFPPsmDZELyphk5hUQQa0WwTGXHmy",
-    access: "original"
+    access: "original",
+    access_hash: "$2b$10$sp5JN3fneiU3amkLcPYDm.YlQUD.EwgN97odovQrIqMJ8h.oulzCu"
   }, {
     id: 2,
     username: "copcop",
     email: "copcop@test.org",
     password_plain: "extremelygoodpassword",
     password_hash: "$2b$10$JanxSBM9JAVi78/5kL3WZOJb/afVUS99MEyAcWEunKXJr/n61SuIe",
-    access: "unique"
+    access: "unique",
+    access_hash: "$2b$10$0lc9ewzIHiX50infkTfNDeLUNbK1jMmJLkbeuQY1NnJ3hosiMzIY."
   }
 ]
 
 let generateAccessToken = payload => {
-  return jwt.sign(payload, ACCESS_TOKEN_SECRET, { expiresIn: "10m" });
+  return jwt.sign(payload, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "10m" });
 }
 
 let generateRefreshToken = payload => {
-  return jwt.sign(payload, REFRESH_TOKEN_SECRET);
+  return jwt.sign(payload, process.env.REFRESH_TOKEN_SECRET);
 }
 
 /* Authentication Paths */
@@ -89,16 +92,38 @@ auth.post('/login', (req, res) => {
   console.log("Posted to login route!");
   if (!req.body.username) return res.status(400).send({missing: "username"});
   if (!req.body.password) return res.status(400).send({missing: "password"});
-  // Here, ideally we would search for the username in the database and check that
-  // the password hash matches the plaintext password.
-  console.log(req.body);
-  let user = tempUsers[1];
-  // We then generate and send a JWT back to the client.
-  let payload = {
-    id: user.id,
-    username: user.username,
-    email: user.email,
-    access: user.access
-  };
-  res.send({accessToken: generateAccessToken(payload), refreshToken: generateRefreshToken(payload)});
+
+  // Search for the user in the database
+  db.query("SELECT * FROM user WHERE username=?;", [req.body.username], (err, result, fields) => {
+    if (err) return res.send(err);
+    console.log(result);
+    if (result.length > 0) {
+      // Check password
+      bcrypt.compare(req.body.password, result[0].password_hash, (err, same) => {
+        if (err) return res.send(err);
+        if (same) { // If the password matches
+          let user = result[0];
+          // Check user permissions
+          bcrypt.compare(process.env.ACCESS1, user.access_hash, (err, same) => {
+            if (err) return res.send(err);
+            let access = same ? process.env.ACCESS1 : process.env.ACCESS2;
+            // We then generate and send a JWT back to the client.
+            let payload = {
+              id: user.id,
+              username: user.username,
+              email: user.email,
+              access: access
+            };
+            console.log(payload);
+            let refreshToken = generateRefreshToken(payload);
+            // Ideally, here we would store the refresh token in a MySQL table before sending both tokens to the user.
+            // Send tokens to user
+            res.send({accessToken: generateAccessToken(payload), refreshToken: refreshToken});
+          });
+        } else return res.send({result: "error", cause: "password"}); // Password incorrect
+      }); 
+    } else return res.send({result: "error", cause: "username"}); // No user with that username
+    //let user = tempUsers[1];
+    
+  });
 });
