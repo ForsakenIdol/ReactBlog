2cd54cab142577abf99ca98387f46c1a240ef05a
ForsakenIdol <lachlandwhang@gmail.com>
2020-12-13 15:12:54 +0800

    String formatting on datetime object for blog posts

--- src/Sections/BlogBox.js
+++ src/Sections/BlogBox.js
@@ -4,7 +4,7 @@ function BlogBox(props) {
     return (
         <div className="container blog-box">
             <h1 className="blog-title">{props.post.title}</h1>
-            <p className="blog-subtitle">{props.author}, {props.post.publish_datetime}</p>
+            <p className="blog-subtitle">{props.author}, {props.post.publish_datetime ? props.post.publish_datetime.split('T')[0] : props.post.publish_datetime}</p>
             <img className="blog-feature-image col-12" style={{padding: 0}} src={props.post.image_link} alt="blogheader" />
             <div style={{fontFamily: "'Raleway', sans-serif", fontSize: "1.05rem", whiteSpace: "pre-line"}}>{props.post.content}</div>
         </div>
